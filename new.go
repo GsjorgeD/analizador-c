13c75ad372f1e9dc46713378ecbf5a71bb17c76c
Jorge Daniel Gutierrez <jdaniel.vivendo@gmail.com>
2021-05-07 20:20:10 -0500

    Add for loop

--- new.go
+++ new.go
@@ -5,6 +5,7 @@ import (
     "io/ioutil"
     "log"
     "strings"
+    "reflect"
 )
 
 func main() {
@@ -17,12 +18,28 @@ func main() {
 
     //fmt.Println(string(content))
 
-    fmt.Println(strings.Contains(string(content), ";"))//hacer una funcion que cheque argumentos
+    verifi := strings.Contains(string(content), "#include")//hacer una funcion que cheque argumentos
+    //fmt.Println(verifi)
+    if verifi==false{
+    fmt.Println("error en include")
+    }
+    pedazos := strings.Split(string(content), " ")
 
-    nombresComoArreglo := strings.Split(string(content), ";")
-
-      fmt.Println(nombresComoArreglo[0])//pofdria hacer una funcion con esto y pasarle el argumento que quier
+      //fmt.Println(pedazos)//pofdria hacer una funcion con esto y pasarle el argumento que quier
 //hacer con while que por cada line cheque hacer para las n ves de lineas que hayya
-//si existe sumar a uno y lklevar la cuenta ty despues comparara
+//si existe sumar a uno y llevar la cuenta y despues comparara
+    numpedazos := len(pedazos)
+    for index := 0; index < numpedazos; index++ {
+    checa("{", string(pedazos[index]))
+    }
 
+
+    checa("#include", string(pedazos[0]))
+    fmt.Println(reflect.TypeOf(verifi))//verififica que tpo de datos es
+
+
+  }
+  func checa(palabra string, content string) {
+    ver := strings.Contains(string(content), palabra)
+    fmt.Println(ver)
   }
